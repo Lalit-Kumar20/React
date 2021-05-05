f754bf23ec3d7cfa49978d5232c34e83f3838782
Lalit-Kumar20 <lalitk2282k@gmail.com>
2021-05-06 01:17:55 +0530

    analog clock is react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React , { useState } from 'react';
 import ReactDOM from 'react-dom';
 import "./styles.css"
+import Clock from './Clock';
 export default function App() {
     var d = new Date();
     var [time,setTime] = useState({
@@ -8,6 +9,7 @@ export default function App() {
         minutes : d.getMinutes(),
         seconds : d.getSeconds() 
     });
+    
     setInterval(function(){ 
          
         var dd = new Date();
@@ -18,11 +20,17 @@ export default function App() {
         };
         console.log(obj);
         setTime(obj);
+        
     }, 1000);
+    
     return (
         <div className="box">
         <h1> {time.hours} : {time.minutes} : {time.seconds} </h1>
-        
+        <Clock 
+            sec = {time.seconds*6}
+            min = {time.minutes*6}
+            hour = {time.hours*30}
+        />
        </div>
     );
   }
---
+++ src/Clock.jsx
@@ -0,0 +1,28 @@
+import React , { useState } from 'react';
+import ReactDOM from 'react-dom';
+import "./styles.css"
+export default function Clock(props){
+    return (
+        <div className= "clk">
+          
+            <div className= "sec" style ={ {transform: `rotate(${props.sec}deg)`}}></div>
+            <div className= "min" style ={ {transform: `rotate(${props.min}deg)`}}></div>
+            <div className= "hour" style ={ {transform: `rotate(${props.hour}deg)`}}></div>
+            <div className= "center"></div> 
+            <h3 className="one">1</h3>
+            <h3 className="two">2</h3>
+            <h3 className="three">3</h3>
+            <h3 className="four">4</h3>
+            <h3 className="five">5</h3>
+            <h3 className="six">6</h3>
+            <h3 className="seven">7</h3>
+            <h3 className="eight">8</h3>
+            <h3 className="nine">9</h3>
+            <h3 className="ten">10</h3>
+            <h3 className="eleven">11</h3>
+            <h3 className="twelve">12</h3>
+
+        </div>
+    );
+
+};
